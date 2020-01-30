acfbfb64b36005020dcfe3c82b288d12b936c1fa
Roger Dodger <lpaddikt@gmail.com>
2020-01-29 23:04:42 -0800

    WIP

---
+++ Client.py
@@ -0,0 +1,15 @@
+samples = [30, 40, 100, 100]
+estimatedRTT = 80
+DevRTT = 0
+ALPHA = 0
+BETA = 0
+
+
+def rtt(samp):
+    estimatedRTT = ((1 - ALPHA) * estimatedRTT) + (ALPHA * sampleRTT)
+    DevRTT = (1 - 0.25) * DevRTT + 0.25 * Math.abs(sampleRTT - estimatedRTT)
+    console.log('ertt', estimatedRTT)
+    console.log('DevRTT', DevRTT)
+
+
+print(list(map(rtt, samples)))
