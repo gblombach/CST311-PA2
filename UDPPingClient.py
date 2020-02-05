b2e2d0c92e346306181aa7d432fef6a4e0cfcbf1
Roger Terrill <rogert@channelbakers.com>
2020-02-04 17:05:49 -0800

    Commented and completed code version 1

--- UDPPingClient.py
+++ UDPPingClient.py
@@ -5,22 +5,28 @@ import time
 
 
 def rtt_equation(sample_arr):
-    EstimatedRTT = 0
-    DevRTT = 0
-    ALPHA = 0.125
-    BETA = 0.25
+    """
+    Calculates and displays the RTT Average, Max, and Min
+    :param sample_arr: List of Samples
+    :return:
+    """
+
+    estimated_rtt = 0
+    dev_rtt = 0
+    alpha = 0.125
+    beta = 0.25
 
     print(list(sample_arr))
     print("========================")
 
     for sampleRTT in sample_arr:
-        EstimatedRTT = (1 - ALPHA) * EstimatedRTT + ALPHA * sampleRTT
-        DevRTT = (1 - BETA) * DevRTT + BETA * abs(sampleRTT - EstimatedRTT)
-    print('EstimatedRTT', round(EstimatedRTT, 3))
-    print("DevRTT", round(DevRTT, 3))
+        estimated_rtt = (1 - alpha) * estimated_rtt + alpha * sampleRTT
+        dev_rtt = (1 - beta) * dev_rtt + beta * abs(sampleRTT - estimated_rtt)
+    print('estimated_rtt', round(estimated_rtt, 3))
+    print("dev_rtt", round(dev_rtt, 3))
     print("========================")
-    Timout = EstimatedRTT + 4 * DevRTT
-    print('TimeoutInterval', round(Timout, 3))
+    timeout_interval = estimated_rtt + 4 * dev_rtt
+    print('TimeoutInterval', round(timeout_interval, 3))
 
 
 serverName = 'localhost'
@@ -30,37 +36,49 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 # set timeout for response
 clientSocket.settimeout(1)
 
-rtt_arr = []
+samples_list = []
 sent = 10
 
 for ping in range(sent):
     try:
+        # Message sent to ping the server
         message = f"This is Ping {ping + 1} {time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime())}"
+
+        # Starting time
         t = time.time()
+
+        # Setup the socket and send the message
         clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+        # Receive the response from the server
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
         print(modifiedMessage.decode())
+
+        # Calculate the elapsed time from when ping was sent
         elapsed_time = (time.time() - t) * 1000
+
+        # Check if ping is returned, if not, print error
         if elapsed_time:
-            rtt_arr.append(elapsed_time)
+            samples_list.append(elapsed_time)
         print(f"RTT is {round(elapsed_time, 3)}ms")
     except timeout as e:
         print("Request timed out")
 clientSocket.close()
 
 sent = 10
-received = len(rtt_arr)
-lost = 10 - len(rtt_arr)
+received = len(samples_list)
+lost = 10 - len(samples_list)
 loss_percentage = (lost/10.) * 100
 
-max_rtt = round(max(rtt_arr), 3)
-min_rtt = round(min(rtt_arr), 3)
-avg_rtt = round(mean(rtt_arr), 3)
+max_rtt = round(max(samples_list), 3)
+min_rtt = round(min(samples_list), 3)
+avg_rtt = round(mean(samples_list), 3)
 
 print("=======================================================")
 print(f"Ping statistics for {serverAddress}: Packets: Sent = {sent}, Received = {received}, Lost = {lost}, "
       f"Loss Percentage = {loss_percentage}% \nApproximate round trip "
       f"times in milli-seconds: Minimum = {min_rtt}ms, Maximum = {max_rtt}ms, Average = {avg_rtt}ms")
 
-rtt_equation(rtt_arr)
+# Display all the rtt values
+rtt_equation(samples_list)
 
